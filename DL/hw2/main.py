7d96b2a29c45b30c8b81408c89c66b38586658ee
fireball0213 <1306189897@qq.com>
2022-12-02 22:51:36 +0800

    complete VAE,CVAE

--- DL/hw2/main.py
+++ DL/hw2/main.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib
 import matplotlib.pyplot as plt
-from torchvision.utils import save_image
+from torchvision.utils import make_grid, save_image
 from data import *
 from model import *
 
@@ -15,120 +15,140 @@ class Loss(nn.Module):
     def __init__(self, decode_type="AE", **kwargs) -> None:
         super().__init__()
         self.decode_type = decode_type
-        #self.epoch=kwargs['epoch']
     def recon_loss(self, x, x_):
         # 实现重构损失函数 (5/100)
+        # L1
+        loss = nn.L1Loss()  # 必须导这么一手
+        recon = loss(x, x_)
         #BCE
-        #recon = F.binary_cross_entropy(F.sigmoid(x_),F.sigmoid(x), reduction='mean')
-        #L1
-        loss = nn.L1Loss()#必须导这么一手
-        recon=loss(x,x_)
+        #recon = F.binary_cross_entropy(F.sigmoid(x_),F.sigmoid(x), reduction='sum')
         return recon
 
-    def kl_div(self, p, q):  # p预测，q目标(y)
+    def kl_div(self, mu,logvar):
         # 实现kl散度的损失 (5/100)
-        log_p = F.log_softmax(p, dim=-1)
-        softmax_q = F.softmax(q, dim=-1)
-        assert (softmax_q[-1].sum() == 1)
-        kl_mean = F.kl_div(log_p, softmax_q, reduction='mean')
-        assert (kl_mean >= 0)
+        kl_mean = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
         return kl_mean
 
-    def forward(self, x, x_, **otherinputs):
+    def forward(self, x, x_,mu,logvar,epoch,epoch_num, **otherinputs):
         # 实现loss的计算, 注意处理 **otherinputs 的解析, 以及不同的decode_type对应于不同的loss (10/100)
-        #kl = Loss.kl_div(self,x, x_)
-        recon=Loss.recon_loss(self,x,x_)
 
+        recon=Loss.recon_loss(self,x,x_)
         if self.decode_type=="AE":
             total_loss=recon
-        # elif self.decode_type=="VAE":
-        #     total_loss=recon+kl#TODO:*annealing
+        elif self.decode_type=="VAE" or self.decode_type=="CVAE":
+            kl = Loss.kl_div(self,mu,logvar)
+            #print(recon,kl)
+            annealing=epoch/epoch_num
+            annealing*=0.003
+            total_loss=recon+kl*annealing
         else:
             raise NotImplementedError
         return total_loss
 
-def train_epoch(model, train_loader, loss, optimizer,epoch, type):
+def train_epoch(model, train_loader, loss, optimizer,epoch, type,epoch_num):
     # 训练过程
     model.train()
     train_loss = 0
     for i, (x, y) in enumerate(train_loader):
         # 训练过程的补全 (20/100) 注意考虑不同类型的AE应该有所区别
         x = x.reshape(128, 1, -1)  # (128,1,784)28*28需要展平
+        loss_num=0
         optimizer.zero_grad()
-        z, x_ = model(x)
-        x = x.requires_grad_(True)
-        x_ = x_.requires_grad_(True)
-        loss_num = loss(x, x_)
+        if type=="AE":
+            z, x_ = model(x)
+            loss_num = loss(x, x_)
+
+        elif  type=="VAE":
+            x_, mu, log_var, z=model(x)
+            loss_num=loss(x, x_,mu,log_var,epoch,epoch_num)
+        elif  type=="CVAE":
+            x_, mu, log_var, z=model(x,y)
+            loss_num=loss(x, x_,mu,log_var,epoch,epoch_num)
+        else:
+            raise NotImplementedError
         train_loss += loss_num
         loss_num.backward()
         optimizer.step()
+
         if i % 30 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, i * len(x), len(train_loader.dataset),
                        100. * i / len(train_loader),
                 loss_num.item())  # / len(x)
             )
-            # 保存一些重构出来的图像用于(写报告)进一步研究 (5/100)
-            # 保存原始数据部分
-            if epoch==0:
-                save_x = x.reshape(128, 28, 28)
-                if not os.path.exists(f"data_fig/{type}/batch_{i}"):
-                    os.makedirs(f"data_fig/{type}/batch_{i}")
-                for j in range(0,128,16):
-                    save_image(save_x[j],
-                        os.path.join(f"data_fig/{type}/batch_{i}", 'origin_' + str(j) + '.png')
-                    )
     print('====> Epoch: {} Average loss: {:.4f}'.format(epoch,
         train_loss * 128 / len(train_loader.dataset)))
     return
 
 
-def test_epoch(model, test_loader, loss, epoch, type):
+def test_epoch(model, test_loader, loss, epoch, type,epoch_num):
     model.eval()
     test_loss = 0
     with torch.no_grad():
         for i, (x, y) in enumerate(test_loader):
             x = x.reshape(128, 1, -1)
-            z, x_ = model(x)
-            test_loss += loss(x, x_).item()
-            save_x = x.reshape(128, 28, 28)
-            save_x_=x_.reshape(128,28,28)
 
-            # 保存一些重构出来的图像用于(写报告)进一步研究 (5/100)
-            with torch.no_grad():
-                if i%30==0:
-                    # 保存原始数据部分
-                    if not os.path.exists(f"results/{type}/epoch_{epoch}/batch_{i}"):
-                        os.makedirs(f"results/{type}/epoch_{epoch}/batch_{i}")
-                    for j in range(0, 128, 16):
-                        save_image(save_x_[j],
-                            os.path.join(f"results/{type}/epoch_{epoch}/batch_{i}", 'train_' + str(j) + '.png')
-                        )
-                    # 保存训练结果部分
-                    if not os.path.exists(f"datas/{type}/batch_{i}"):
-                        os.makedirs(f"datas/{type}/batch_{i}")
-                    for j in range(0, 128, 16):
-                        save_image(save_x[j],
-                            os.path.join(f"datas/{type}/batch_{i}", 'train_' + str(j) + '.png')
-                        )
+            if type == "AE":
+                z, x_ = model(x)
+                loss_num = loss(x, x_)
+                test_loss += loss_num.item()
+
+                # 保存一些重构出来的图像用于(写报告)进一步研究 (5/100)
+                with torch.no_grad():
+                    if i%30==0:
+                        # 保存原始数据部分
+                        if not os.path.exists(f"datas/{type}"):
+                            os.makedirs(f"datas/{type}")
+                        save_x = x.reshape(-1,1, 28, 28)[:16]
+                        save_x = make_grid(save_x,8,0)
+                        save_image(save_x,os.path.join(f"datas/{type}/batch_{i}.png"))
+
+                        # 保存训练结果部分
+                        if not os.path.exists(f"results/{type}/epoch_{epoch}"):
+                            os.makedirs(f"results/{type}/epoch_{epoch}")
+                        save_x_ = x_.reshape(-1, 1, 28, 28)[:16]
+                        save_x_ = make_grid(save_x_, 8, 0)
+                        save_image(save_x_, os.path.join(f"results/{type}/epoch_{epoch}/batch_{i}.png"))
+
+            elif type == "VAE":
+                x_, mu, log_var, z = model(x)
+                loss_num = loss(x, x_, mu, log_var, epoch, epoch_num)
+                test_loss += loss_num.item()
+                z = torch.randn(128,1,10)
+                sample = model.inference(z)
+                sample = sample.reshape(-1, 1, 28, 28)[:32]
+                print(z[0],sample[0])
+                # 保存一些重构出来的图像用于(写报告)进一步研究 (5/100)
+                if not os.path.exists(f"results/{type}"):
+                    os.makedirs(f"results/{type}")
+                save_image(
+                    sample,
+                    os.path.join(f"results/{type}/epoch_{epoch}.png")
+                )
+            elif type == "CVAE":
+                x_, mu, log_var, z = model(x,y)
+                loss_num = loss(x, x_, mu, log_var, epoch, epoch_num)
+                test_loss += loss_num.item()
+                z = torch.randn(128,1,10)
+                sample = model.inference(z,y)
+                sample = sample.reshape(-1, 1, 28, 28)[:32]
+                print(z[0],sample[0])
+                # 保存一些重构出来的图像用于(写报告)进一步研究 (5/100)
+                if not os.path.exists(f"results/{type}"):
+                    os.makedirs(f"results/{type}")
+                save_image(
+                    sample,
+                    os.path.join(f"results/{type}/epoch_{epoch}.png")
+                )
+
+            else:
+                raise NotImplementedError
+
     test_loss*=128
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
-
-# def train(model, train_loader,test_loader, loss, optimizer, epoch_num, type):  #
-#
-#         # 保存一些重构出来的图像用于(写报告)进一步研究 (5/100)
-#         # 保存图像的部分，参见train_epoch和test_epoch
-#     return
-
 def main(args):
-    # encoder_args = {
-    #     "x_dim": args.x_dim,
-    #     "hidden_size": args.hidden_size,
-    #     "latent_size": args.latent_size,
-    #     "is_dist": True if args.type in ["VAE", "CVAE"] else False,
-    # }
     encoder_args = {
         "x_dim": args.x_dim,
         "hidden_size": args.hidden_size,
@@ -141,8 +161,8 @@ def main(args):
     ae = {"AE": AutoEncoder, "VAE": VariationalAutoEncoder, "CVAE": ConditionalVariationalAutoEncoder}
 
     auto_encoder = ae[args.type](encoder, decoder)
+
     # 挑选你喜欢的优化器 :)
-    # print("模型参数：", list((auto_encoder.parameters())))
     #optimizer = torch.optim.SGD(auto_encoder.parameters(), lr=0.01)
     optimizer =torch.optim.Adam(auto_encoder.parameters(), lr=0.001)
     train_loader = get_data(train=True, batch_size=args.batch_size)
@@ -153,20 +173,23 @@ def main(args):
     #       optimizer=optimizer, epoch_num=args.epoch_num,type=args.type)
     for epoch in range(args.epoch_num):
         print("\n epoch:", epoch)
+
         train_epoch(model=auto_encoder, loss=loss, train_loader=train_loader,
-                    optimizer=optimizer, epoch=epoch, type=args.type)
-        test_epoch(model=auto_encoder,test_loader=test_loader, loss=loss, epoch=epoch, type=args.type)
+                    optimizer=optimizer, epoch=epoch, type=args.type,epoch_num=args.epoch_num)
+        test_epoch(model=auto_encoder, test_loader=test_loader, loss=loss,
+                   epoch=epoch, type=args.type, epoch_num=args.epoch_num)
+
 
 
 if __name__ == "__main__":
     matplotlib.use('TkAgg')
     parser = argparse.ArgumentParser()
-    parser.add_argument("--type", default="AE", choices=["AE", "VAE", "CVAE"])
+    parser.add_argument("--type", default="CVAE", choices=["AE", "VAE", "CVAE"])
     parser.add_argument("--x_dim", default=784, type=int)  # 28 x 28 的像素展开为一个一维的行向量，每行代表一个图片
     parser.add_argument("--latent_size", default=10, type=int)  # 输出层大小，即服从高斯分布的隐含变量的维度。
     parser.add_argument("--hidden_size", default=128, type=int)
     parser.add_argument("--batch_size", default=128, type=int)
-    parser.add_argument("--epoch_num", default=10, type=int)
+    parser.add_argument("--epoch_num", default=8, type=int)
     args = parser.parse_args()
     print(args)
     main(args)
--- DL/hw2/model.py
+++ DL/hw2/model.py
@@ -9,41 +9,67 @@ import torch.nn.functional as F
     - Conditional Variational AutoEncoder
     
 """
-
+def idx2onehot(idx, n):
+    assert torch.max(idx).item() < n
+    if idx.dim() == 1:
+        idx = idx.unsqueeze(1)
+    onehot = torch.zeros(idx.size(0), n)
+    onehot.scatter_(1, idx, 1)
+    return onehot
 
 class Encoder(nn.Module):
-    def __init__(self, x_dim, hidden_size, latent_size, is_dist=False, **kwargs) -> None:
+    def __init__(self, x_dim, hidden_size, latent_size, is_dist=False, conditional=False, **kwargs) -> None:
         super(Encoder, self).__init__()
         self.mu = nn.Sequential(nn.Linear(x_dim, hidden_size), nn.ReLU(), nn.Linear(hidden_size, latent_size), )
         self.is_dist=is_dist
+        self.conditional=conditional
         if self.is_dist:  # 如果需要encoder返回的是均值与标准差, 那么我们需要额外引入一次计算标准差的layer
             self.sigma = nn.Sequential(nn.Linear(x_dim, hidden_size), nn.ReLU(), nn.Linear(hidden_size, latent_size), )
 
-    def forward(self, xs):
+    def forward(self, xs, c=None):
         # 实现编码器的forward过程 (5/100), 注意 is_dist 的不同取值意味着我们需要不同输出的encoder
         enc_outputs = self.mu(xs)
         if self.is_dist:
-            means = self.self.sigma(xs)#使用不同网络
-            log_vars = self.self.sigma(xs)
-            return means,log_vars
+            if self.conditional:
+                c = idx2onehot(c, n=10)
+                xs= torch.cat((xs, c), dim=-1)
+            mu = self.sigma(xs)#使用不同网络
+            sigma = self.sigma(xs)
+            return mu,sigma
         return enc_outputs  # (128,1,10)
 
 
 class Decoder(nn.Module):
-    def __init__(self, x_dim, hidden_size, latent_size, decode_type="AE", **kwargs) -> None:
+    def __init__(self, x_dim, hidden_size, latent_size, decode_type="AE", conditional=False, **kwargs) -> None:
         super(Decoder, self).__init__()
+        self.conditional = conditional
         if decode_type == "AE":
             self.decoder = nn.Sequential(nn.Linear(latent_size, hidden_size), nn.ReLU(),
                                          nn.Linear(hidden_size, x_dim), )
         elif decode_type == "VAE":
             self.decoder = ...  # 实现VAE的decoder (5/100)
+            self.decoder = nn.Sequential(nn.Linear(latent_size, 50), nn.ReLU(),
+                                         nn.Linear(50, hidden_size), nn.ReLU(),
+                                         nn.Linear(hidden_size, 400), nn.ReLU(),
+                                         nn.Linear(400, x_dim)
+                                         )
         elif decode_type == "CVAE":
             self.decoder = ...  # 实现CVAE的decoder (5/100)
+            self.decoder = nn.Sequential(nn.Linear(latent_size, 32), nn.ReLU(),
+                                         nn.Linear(32, 64), nn.ReLU(),
+                                         nn.Linear(64, hidden_size), nn.ReLU(),
+                                         nn.Linear(hidden_size, 256), nn.ReLU(),
+                                         nn.Linear(256, 400), nn.ReLU(),
+                                         nn.Linear(400, x_dim)
+                                         )
         else:
             raise NotImplementedError
 
-    def forward(self, zs, **otherinputs):
+    def forward(self, zs,c=None, **otherinputs):
         # 实现decoder的decode部分, 注意对不同的decode_type的处理与对**otherinputs的解析 (10/100)
+        if self.conditional:
+            c = idx2onehot(c, n=10)
+            zs = torch.cat((zs, c), dim=-1)
         dec_outputs = self.decoder(zs)
         return dec_outputs  # (128,1,784)
 
@@ -69,10 +95,19 @@ class VariationalAutoEncoder(nn.Module):
         self.decoder = decoder
 
     def forward(self, xs):
-        mu, sigma = self.encoder(xs)
         # 实现VAE的forward过程(10/100)
-        ...
-        return
+        mu, log_var = self.encoder(xs)
+
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        z = eps * std + mu
+
+        x_ = self.decoder(z)
+
+        return x_, mu,log_var, z
+    def inference(self, z):
+        x_ = self.decoder(z)
+        return x_
 
 
 class ConditionalVariationalAutoEncoder(nn.Module):
@@ -82,7 +117,17 @@ class ConditionalVariationalAutoEncoder(nn.Module):
         self.decoder = decoder
 
     def forward(self, xs, ys):
-        mu, sigma = self.encoder(xs, ys)
         # 实现 CVAE的forward过程(15/100)
-        ...
-        return
+        mu, log_var = self.encoder(xs,ys)
+
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        z = eps * std + mu
+
+        x_ = self.decoder(z,ys)
+
+        return x_, mu, log_var, z
+
+    def inference(self, z,y):
+        x_ = self.decoder(z,y)
+        return x_
